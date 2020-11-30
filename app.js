c616dc42c9237c0310f7b9406c01ac439ad0b205
Shivansh Gupta <mail@shivanshgupta.com>
2020-11-30 13:37:56 +0530

    Project Completed

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('hello shivansh')
+const fs = require('fs');
+const data = fs.writeFileSync ('leopard.txt', 'Leopard is the fastest animal in the animal kingdom')
+// const data = fs.readFileSync ('lions.txt')
+// const DataS = data.toString();
+// console.log(DataS)
